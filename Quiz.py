a5e1def37fc60c1297aed5cd24e351a381e6e2d7
Dávid Soóky <sooky.david98@gmail.com>
2022-09-27 13:17:17 +0200

    Update Quiz.py
    lower() method works.

--- Quiz.py
+++ Quiz.py
@@ -1,6 +1,6 @@
 print("Welcome to my computer quiz!")
 
-playing = input("Do you want to play? ")
+playing = input("Do you want to play? ").lower()
 if playing != "yes":
 	print("Okay! Have a great day:)")
 	quit()
@@ -9,28 +9,28 @@ NumberOfPoints=0
 
 print("Okay! Let's play :)")	
 
-answear = input("What does CPU stand for? ")
+answear = input("What does CPU stand for? ").lower()
 if answear == "central processing unit":
 	print("Correct!")
 	NumberOfPoints+=1
 else:
 	print("Incorrect!")
 
-answear = input("What does GPU stand for? ")
+answear = input("What does GPU stand for? ").lower()
 if answear == "graphics processing unit":
 	print("Correct!")
 	NumberOfPoints+=1
 else:
 	print("Incorrect!")
 
-answear = input("What does RAM stand for? ")
+answear = input("What does RAM stand for? ").lower()
 if answear == "random access memory":
 	print("Correct!")
 	NumberOfPoints+=1
 else:
 	print("Incorrect!")
 
-answear = input("What does PSU stand for? ")
+answear = input("What does PSU stand for? ").lower()
 if answear == "power supply unit":
 	print("Correct!")
 	NumberOfPoints+=1
